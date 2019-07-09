614cfa9d96df267c220dd33f9aa03809165e701c
marfrede <marvin.frede@hs-osnabrueck.de>
2019-07-09 17:35:07 +0200

    added breeds to cats view

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -2,11 +2,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cat;
+use App\Models\Breed;
 
 class CatController extends Controller
 {
     public function getIndex()
     {
-        return view('cats.index')->with('cats',Cat::all());
+        return view('cats.index')->with('cats',Cat::all())->with('breeds',Breed::all());
     }
 }
