ed21ae9c0f21e23434619205b2207f379f910d23
marfrede <marvin.frede@hs-osnabrueck.de>
2019-07-10 08:19:33 +0200

    added breed column to cats table

--- app/Http/Controllers/PackController.php
+++ app/Http/Controllers/PackController.php
@@ -9,8 +9,4 @@ class PackController extends Controller
     {
         return view('packs.index')->with('packs',Pack::all());
     }
-
-    public function delete($id){
-        Pack::where('id',$id)->delete();
-    }
 }
