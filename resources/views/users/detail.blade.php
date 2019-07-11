1f25f9b0a61380ad1c4108e9d6afba046f4470b4
marfrede <marvin.frede@hs-osnabrueck.de>
2019-07-11 10:21:02 +0200

    added detail pages

--- resources/views/users/detail.blade.php
+++ resources/views/users/detail.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 
+<h3>User</h3>
 <h1>{{$user->name}}</h1>
+<div class="content">
+    <table>
+        <tbody>
+            <tr>
+                <th scope="row">ID:</th>
+                <td>{{$user->id}}</td>
+            </tr>
+            <tr>
+                <th scope="row">created at:</th>
+                <td>{{$user->created_at}}</td>
+            </tr>
+            <tr>
+                <th scope="row">updated at:</th>
+                <td>{{$user->updated_at}}</td>
+            </tr>
+            <tr>
+                <th scope="row">email:</th>
+                <td>{{$user->email}}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 @endsection
