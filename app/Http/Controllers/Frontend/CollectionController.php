2c81bc173a5979d1437283d178e82ad3cafd74f9
Nils Geschwinde <nils.geschwinde@newroom-media.de>
2019-07-12 10:38:56 +0200

    route fixes

--- app/Http/Controllers/Frontend/CollectionController.php
+++ app/Http/Controllers/Frontend/CollectionController.php
@@ -11,7 +11,9 @@ use Auth;
 class CollectionController extends Controller
 {
     function getIndex(){
-        return view('frontend.collection.index')->with('cats', Auth::user()->cats()->paginate(6));
+        return view('frontend.collection.index')->with('cats', 
+        Auth::user()->cats()->join('breeds', 'cats.breed_id', '=', 'breeds.id')->join('rarities', 'breeds.rarity_id', '=', 'rarities.id')->orderBy('rarities.chance')->paginate(6));
+        // User::select('users.*')->leftJoin()
     }
 
     function postStartQuest(){
